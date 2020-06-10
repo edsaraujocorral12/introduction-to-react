7021be224dea2754002332217f5f39b5d3f8ada0
Edson <edsaraujocorral12@gmail.com>
2020-06-10 15:34:46 -0300

    Removendo Itens do State

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -15,18 +15,33 @@ class TechList extends Component {
   }
   handleSubmit = e => {
     e.preventDefault(); // Previnir o comportamento padrao
-
+    if (this.state.newTech === '') {
+      return;
+    }
     this.setState({
       techs: [...this.state.techs, this.state.newTech],
       newTech: '',
     });
   }
 
+  handleDelete(tech) {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button
+                type="button"
+                onClick={() => this.handleDelete(tech)}>
+                Remover
+              </button>
+            </li>
+          ))}
         </ul>
         <input
           type="text"
