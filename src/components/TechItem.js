53f71b0a1e23986ca19ba3e0766449f08febb851
Edson <edsaraujocorral12@gmail.com>
2020-06-10 15:49:10 -0300

    Trabalhando com Propriedades

---
+++ src/components/TechItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TechItem({ tech, onDelete }) {
+  return (
+    <li>
+      {tech}
+      <button
+        type="button"
+        onClick={onDelete}>
+        Remover
+    </button>
+    </li>);
+}
+
+export default TechItem;
